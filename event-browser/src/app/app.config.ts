b2e1e948a94520127003a6f7442c1f534d988320
Ben Field <fieldben97@gmail.com>
2024-11-22 18:22:19 +1100

    added styling

--- event-browser/src/app/app.config.ts
+++ event-browser/src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()), provideHttpClient()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }), 
+    provideRouter(routes), 
+    provideClientHydration(withEventReplay()), 
+    provideHttpClient(withFetch())
+  ]
 };
--- event-browser/src/app/event-list/event-list.component.ts
+++ event-browser/src/app/event-list/event-list.component.ts
@@ -4,6 +4,7 @@ import { Component, Input } from '@angular/core';
 @Component({
   selector: 'app-event-list',
   templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css'],
   standalone: true,
   imports: [
     CommonModule,
