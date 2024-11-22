07e675417d347b5e9d759485a6dae3823a73f88c
Ben Field <fieldben97@gmail.com>
2024-11-22 16:39:44 +1100

    added basic search form component

--- event-browser/src/app/search-form/search-form.component.ts
+++ event-browser/src/app/search-form/search-form.component.ts
@@ -7,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrl: './search-form.component.css'
 })
 export class SearchFormComponent {
+  location: string = '';
+  startDate: string = '';
+  endDate: string = '';
 
+  onSearch() {
+    // Will implement API call here later
+  }
 }
